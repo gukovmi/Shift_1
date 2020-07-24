c0571aca3cf8af107012f1ab2b384f99c8697d37
gukovmi <68592103+gukovmi@users.noreply.github.com>
2020-07-24 12:44:15 +0700

    improved logic

--- src/app.kt
+++ src/app.kt
@@ -1,21 +1,22 @@
-import java.lang.Exception
+import kotlin.Exception
 
 fun main(args: Array<String>){
     print("What's your current city?")
-    val currentCity:String = readLine()!!
+    val currentCity:String = readLine() ?: ""
+    if (currentCity.isEmpty()) {
+        print(println("Fields filled in incorrectly, please try again!"))
+        return
+    }
 
     print("What is the temperature outside?")
-    val outsideTemperature:String = readLine()!!
-
-    try {
-        when (outsideTemperature.toInt()) {
-            in -50..15->{println("It's cold in $currentCity now")}
-            in 15..25->{println("It's normal in $currentCity now")}
-            in 25..50->{println("It's hot in $currentCity now")}
-            else->{println("It's disastrously in $currentCity now")}
-        }
-    }
-    catch (e:Exception) {
-        println("Fields filled in incorrectly, please try again!")
+    val outsideTemperature:String = readLine() ?: ""
+    when (
+        try {outsideTemperature.toInt()}
+        catch (e:Exception) {null}) {
+        in -50..15 -> println("It's cold in $currentCity now")
+        in 15..25 -> println("It's normal in $currentCity now")
+        in 25..50 -> println("It's hot in $currentCity now")
+        null->println("Fields filled in incorrectly, please try again!")
+        else -> println("It's disastrously in $currentCity now")
     }
 }
